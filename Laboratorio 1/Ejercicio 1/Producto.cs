3b00d460b544abab27ca584df89371d2be05b2c4
Pazzis Paulino <1103790@est.intec.edu.do>
2023-02-03 14:21:28 -0400

    First Assignment done

---
+++ Laboratorio 1/Ejercicio 1/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Producto
+    {
+        private string nombre;
+        private double precio;
+        private string descripcion;
+        private int stock;
+        private double itbis;
+
+        public Producto(string nombre, double precio, string descripcion, int stock, double itbis)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.descripcion = descripcion;
+            this.stock = stock;
+            this.itbis = itbis;
+        }
+        
+        public string RetornarDatos()
+        {
+            string mensaje = "Los datos del producto son: \nNombre: " + nombre + "\nPrecio: " + precio + "\nDescripcion: " + descripcion + "\nStock: " + stock + "\nITBIS: " + itbis;
+            return mensaje;
+        }
+    }
+}
---
+++ Laboratorio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Autor: Pazzis Paulino
+   Laboratorio 1. Ejercicio 1.
+   Mandato: Crear una clase con propiedades. Instanciar la clase en un objeto. 
+            Asignarle valores Mostrar los valores de las propiedades del objeto 
+            por la pantalla consola.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A continuacion, ingrese uno a uno los datos del producto: ");
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Precio: ");
+            double precio = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Descripcion: ");
+            string descripcion = Console.ReadLine();
+            Console.Write("Stock: ");
+            int stock = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ITBIS: ");
+            double itbis = Convert.ToDouble(Console.ReadLine());
+            
+            //Instanciacion del objeto de la clase Producto
+            Producto producto = new Producto(nombre, precio, descripcion, stock, itbis);
+            
+            
+            Console.WriteLine(producto.RetornarDatos());
+            
+
+        }
+    }
+}
