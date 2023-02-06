e14e4b43363961f6f41621f5ad021a9df03867eb
Pazzis Paulino <1103790@est.intec.edu.do>
2023-02-06 14:16:22 -0400

    Cambios listos

---
+++ Laboratorio 1/Ejercicio 2/Infidelidades.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Infidelidades
+    {
+        public int Id { get; set; }
+        public string NombreAfectado { get; set; }
+        public string ApellidosAfectado { get; set; }
+        public string NombreInfiel { get; set; }
+        public string ApellidosInfiel { get; set; }
+        public string SexoInfiel { get; set; }
+        public string SexoAfectado { get; set; }
+        public DateTime FechaEvento { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string EstadoPareja { get; set; }
+        public bool EsLaPrimeraVez { get; set; }
+
+        public string RetornarDatos()
+        {
+            //Retornar cada atributo de esta clase
+            return "Id de la infidelidad: " + this.Id + "\n" +
+                "--------------------------------------" + "\n" +
+                "Nombre del Afectado: " + this.NombreAfectado + "\n" +
+                "Apellidos del Afectado: " + this.ApellidosAfectado + "\n" +
+                "Nombre del Infiel: " + this.NombreInfiel + "\n" +
+                "Apellidos del Infiel: " + this.ApellidosInfiel + "\n" +
+                "Sexo del Infiel: " + this.SexoInfiel + "\n" +
+                "Sexo del Afectado: " + this.SexoAfectado + "\n" +
+                "Fecha del Evento: " + this.FechaEvento.ToShortDateString() + "\n" +
+                "Fecha de Ingreso: " + this.FechaIngreso.ToShortDateString() + "\n" +
+                "Estado de la Pareja: " + this.EstadoPareja + "\n" +
+                "Es La Primera Vez?: " + (this.EsLaPrimeraVez ? "Si" : "No") + "\n";
+        }
+        public void Insertar(){
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("InsertarInfidelidad", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@NombreAfectado", this.NombreAfectado);
+            cmd.Parameters.AddWithValue("@ApellidosAfectado", this.ApellidosAfectado);
+            cmd.Parameters.AddWithValue("@NombreInfiel", this.NombreInfiel);
+            cmd.Parameters.AddWithValue("@ApellidosInfiel", this.ApellidosInfiel);
+            cmd.Parameters.AddWithValue("@SexoInfiel", this.SexoInfiel);
+            cmd.Parameters.AddWithValue("@SexoAfectado", this.SexoAfectado);
+            cmd.Parameters.AddWithValue("@FechaEvento", this.FechaEvento);
+            cmd.Parameters.AddWithValue("@EstadoPareja", this.EstadoPareja);
+            cmd.Parameters.AddWithValue("@EsLaPrimeraVez", this.EsLaPrimeraVez);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+
+    }
+}
--- Laboratorio 1/Ejercicio 2/Program.cs
+++ Laboratorio 1/Ejercicio 2/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿/* Autor: Pazzis Paulino
+   Laboratorio 1. Ejercicio 2.
+   Mandato: Crear una aplicacion para registrar las infidelidades
+*/
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +16,86 @@ namespace Ejercicio_2
     {
         static void Main(string[] args)
         {
+
+            Infidelidades obj = new Infidelidades();
+
+            try
+            {
+                char continuar = (char)13;
+                do
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("A continuacion, ingrese los datos de la infidelidad: ");
+
+                    Console.Write("Nombre del afectado: ");
+                    obj.NombreAfectado = Console.ReadLine();
+
+                    Console.Write("Apellido del afectado: ");
+                    obj.ApellidosAfectado = Console.ReadLine();
+
+                    Console.Write("Sexo del afectado: ");
+                    obj.SexoAfectado = Console.ReadLine();
+
+                    Console.Write("Nombre del infiel: ");
+                    obj.NombreInfiel = Console.ReadLine();
+
+                    Console.Write("Apellido del infiel: ");
+                    obj.ApellidosInfiel = Console.ReadLine();
+
+                    Console.Write("Sexo del infiel: ");
+                    obj.SexoInfiel = Console.ReadLine();
+
+                    Console.Write("Fecha de la infidelidad (mm/dd/aaaa): ");
+                    obj.FechaEvento = DateTime.Parse(Console.ReadLine());
+
+                    Console.Write("Estado de la pareja: ");
+                    obj.EstadoPareja = Console.ReadLine();
+
+                    Console.Write("Es la primera vez? (S/N): ");
+                    char PrimeraVez = Console.ReadLine().ToUpper()[0];
+                    obj.EsLaPrimeraVez = (PrimeraVez == 'S') ? true : false;
+
+                    obj.Insertar();
+
+                    Console.Write("Si desea ingresar otro registro, presione ENTER... ");
+                    continuar = Console.ReadKey().KeyChar;
+
+                } while (continuar == (char)13);
+
+                Console.Clear();
+
+                Console.WriteLine("------INFIDELIDADES REGISTRADAS EN LA BASE DE DATOS----------");
+                Console.WriteLine("-------------------------------------------------------------");
+
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+                conn.Open();
+
+                SqlDataReader dr = new SqlCommand("SELECT * FROM Infidelidades", conn).ExecuteReader();
+                while (dr.Read())
+                {
+                    obj = new Infidelidades();
+                    obj.Id = (int)dr["Id"];
+                    obj.NombreAfectado = dr["NombreAfectado"].ToString();
+                    obj.ApellidosAfectado = dr["ApellidosAfectado"].ToString();
+                    obj.NombreInfiel = dr["NombreInfiel"].ToString();
+                    obj.ApellidosInfiel = dr["ApellidosInfiel"].ToString();
+                    obj.SexoInfiel = dr["SexoInfiel"].ToString();
+                    obj.SexoAfectado = dr["SexoAfectado"].ToString();
+                    obj.FechaEvento = (DateTime)dr["FechaEvento"];
+                    obj.FechaIngreso = (DateTime)dr["FechaIngreso"];
+                    obj.EstadoPareja = dr["EstadoPareja"].ToString();
+                    obj.EsLaPrimeraVez = (bool)dr["EsLaPrimeraVez"];
+
+                    Console.WriteLine(obj.RetornarDatos());
+                    Console.WriteLine("..............................................\n");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
