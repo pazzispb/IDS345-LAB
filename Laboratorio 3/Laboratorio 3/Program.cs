f8bcc7f74a839ba07a3057926b0ec8e82b94eb55
Pazzis Paulino <1103790@est.intec.edu.do>
2023-02-17 01:54:17 -0400

    Laboratorio 3 listo

---
+++ Laboratorio 3/Laboratorio 3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Laboratorio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char continuar = (char)13;
+            clsFallecidos obj;
+            do
+            {
+                obj = new clsFallecidos();
+                Console.Clear();
+
+                
+                Console.WriteLine("A continuacion, ingrese los datos del muerto: ");
+
+                Console.Write("Pais: ");
+                obj.Pais = Console.ReadLine();
+
+                Console.Write("Ciudad: ");
+                obj.Ciudad = Console.ReadLine();
+
+                SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\pazzi\\OneDrive - INTEC\\Escritorio\\DESARROLLO III\\LABORATORIO\\TAREAS LAB-IDS345\\Laboratorio 3\\Laboratorio 3\\Laboratorio3-DB.mdf\";Integrated Security=True");
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("ppGetCiudad", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Pais", obj.Pais);
+                cmd.Parameters.AddWithValue("@Ciudad", obj.Ciudad);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    Console.WriteLine("Cantidad de hombres en la ciudad: " + reader["CantidadHombres"].ToString());
+                    Console.WriteLine("Cantidad de mujeres en la ciudad: " + reader["CantidadMujeres"].ToString());
+                }
+
+                reader.Close();
+
+                con.Close();
+
+                Console.Write("Tipo Documento: ");
+                obj.TipoDocumento = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Documento: ");
+                obj.Documento = Console.ReadLine();
+
+                Console.Write("Nombres: ");
+                obj.Nombres = Console.ReadLine();
+
+                Console.Write("Apellidos: ");
+                obj.Apellidos = Console.ReadLine();
+
+                Console.Write("Fecha de Nacimiento (yyyy-mm-dd): ");
+                obj.FechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+
+                Console.Write("Sexo: ");
+                obj.Sexo = Console.ReadLine();
+                
+               
+
+                obj.InsertarFallecido();
+
+                Console.Write("Si desea ingresar otro registro, presione ENTER... ");
+                continuar = Console.ReadKey().KeyChar;
+
+            } while (continuar == (char)13);
+
+            Console.Clear();
+        }
+    }
+}
---
+++ Laboratorio 3/Laboratorio 3/clsFallecidos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3
+{
+    internal class clsFallecidos
+    {
+        public int Id { get; set; }
+        public int TipoDocumento { get; set; }
+        public string Documento { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Sexo { get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public int Estado { get; set; }
+
+        public void InsertarFallecido()
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\pazzi\\OneDrive - INTEC\\Escritorio\\DESARROLLO III\\LABORATORIO\\TAREAS LAB-IDS345\\Laboratorio 3\\Laboratorio 3\\Laboratorio3-DB.mdf\";Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("ppInsertarFallecidos", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@TipoDocumento", TipoDocumento);
+            cmd.Parameters.AddWithValue("@Documento", Documento);
+            cmd.Parameters.AddWithValue("@Nombres", Nombres);
+            cmd.Parameters.AddWithValue("@Apellidos", Apellidos);
+            cmd.Parameters.AddWithValue("@FechaNacimiento", FechaNacimiento);
+            cmd.Parameters.AddWithValue("@Sexo", Sexo[0]);
+            cmd.Parameters.AddWithValue("@Pais", Pais);
+            cmd.Parameters.AddWithValue("@Ciudad", Ciudad);
+            cmd.Parameters.AddWithValue("@Estado", Estado);
+
+            SqlTransaction tran = con.BeginTransaction();
+            cmd.Transaction = tran;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                
+                cmd.Parameters.Clear();
+                cmd.CommandText = "ppUpsertResumen";
+                cmd.Parameters.AddWithValue("@Pais", Pais);
+                cmd.Parameters.AddWithValue("@Ciudad", Ciudad);
+                cmd.Parameters.AddWithValue("@Sexo", Sexo[0]);
+
+                cmd.ExecuteNonQuery();
+
+                tran.Commit();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Hubo un error en la operacion, los cambios se revirtieron");
+                tran.Rollback();
+            }
+        }
+
+    }
+}
