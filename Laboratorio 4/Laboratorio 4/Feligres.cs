6225b6191d6ca25d3a07ba566c745e586d6f126b
Pazzis Paulino <1103790@est.intec.edu.do>
2023-02-24 00:30:07 -0400

    Laboratorio 4 terminado

---
+++ Laboratorio 4/Laboratorio 4/Feligres.cs
@@ -0,0 +1,61 @@
+﻿using Laboratorio_4.MyDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_4
+{
+    internal class Feligres
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public int Id { get; set; }
+        public int TipoDocumento { get; set; }
+        public string Documento { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime FechaUltimaConfesion { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Sexo { get; set; }
+        public bool Diezmo { get; set; }
+        public bool PerteneceComunidad { get; set; }
+        public DateTime UltimaVisitaIglesia { get; set; }
+
+        public void InsertarFeligres(int TipoEvento, string Nota)
+        {
+            int TipEv = TipoEvento;
+            tblFeligresesTableAdapter feligres = new tblFeligresesTableAdapter();
+            feligres.Connection.Open();
+            SqlTransaction transaction = feligres.Connection.BeginTransaction();
+            feligres.Transaction = transaction;
+            try
+            {
+                feligres.ppInsertarFeligreses(TipoDocumento, Documento, Nombres, Apellidos, FechaNacimiento, FechaUltimaConfesion, EstadoCivil, Sexo, Diezmo, PerteneceComunidad, UltimaVisitaIglesia, TipoEvento);
+
+                string descripcion = "";
+                if (TipoEvento == 1) descripcion = "Mortal";
+                else if (TipoEvento == 2) descripcion = "Venial";
+                else
+                {
+                    TipEv = 3;
+                    descripcion = "Penitencia";
+                }
+
+                feligres.ppInsertEvento(TipoDocumento, Documento, TipoEvento, Nota, descripcion);
+                transaction.Commit();
+                log.Info("Transaccion confirmada");
+            }
+            catch(Exception e)
+            {
+                transaction.Rollback();
+                log.Error("Transaccion revertida debido a errores");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ Laboratorio 4/Laboratorio 4/Program.cs
@@ -0,0 +1,81 @@
+﻿using Laboratorio_4.MyDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char continuar = (char)13;
+            do
+            {
+                Console.Clear();
+                Feligres obj = new Feligres();
+
+                Console.WriteLine("A continuacion, ingrese el las informaciones del feligres: ");
+
+                Console.Write("Ingrese el tipo de documento: ");
+                obj.TipoDocumento = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el documento: ");
+                obj.Documento = Console.ReadLine();
+
+                tblFeligresesTableAdapter feligres = new tblFeligresesTableAdapter();
+
+                if ((int)feligres.ppGetFeligresCount(obj.TipoDocumento, obj.Documento) == 0)
+                {
+                    Console.Write("Ingrese el nombre: ");
+                    obj.Nombres = Console.ReadLine();
+
+                    Console.Write("Ingrese el apellido: ");
+                    obj.Apellidos = Console.ReadLine();
+
+                    Console.Write("Ingrese la fecha de nacimiento (yyyy/mm/dd): ");
+                    obj.FechaNacimiento = DateTime.Parse(Console.ReadLine());
+
+                    Console.Write("Ingrese la fecha de ultima confesion (yyyy/mm/dd): ");
+                    obj.FechaUltimaConfesion = DateTime.Parse(Console.ReadLine());
+
+                    Console.Write("Ingrese el estado civil: ");
+                    obj.EstadoCivil = Console.ReadLine();
+
+                    Console.Write("Ingrese el sexo: ");
+                    obj.Sexo = Console.ReadLine();
+
+                    Console.Write("Ingrese si diezma (S/N): ");
+                    obj.Diezmo = Console.ReadLine().ToLower()[0] == 's' ? true : false;
+
+                    Console.Write("Ingrese si pertenece a una comunidad (S/N): ");
+                    obj.PerteneceComunidad = Console.ReadLine().ToLower()[0] == 's' ? true : false;
+
+                    Console.Write("Ingrese la fecha de ultima visita a la iglesia (yyyy/mm/dd):: ");
+                    obj.UltimaVisitaIglesia = DateTime.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    obj.FechaNacimiento = DateTime.Now;
+
+                    obj.FechaUltimaConfesion = DateTime.Now;
+
+                    obj.UltimaVisitaIglesia = DateTime.Now;
+                }
+                Console.Write("Ingrese el tipo de evento (1 - Pecado Mortal, 2 - Pecado Venial, 3 - Penitencia): ");
+                int TipoEvento = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese una nota sobre el evento: ");
+                string Nota = Console.ReadLine();
+
+                obj.InsertarFeligres(TipoEvento, Nota);
+                
+                Console.Write("Si desea ingresar otro registro, presione ENTER... ");
+                continuar = Console.ReadKey().KeyChar;
+            
+            } while (continuar == (char)13);
+        }
+    }
+}
